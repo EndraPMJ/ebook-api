071717e454fca93f2c64cd32d19348cffbbf5821
unknown <akiranation123@gmail.com>
2020-08-06 12:21:24 -0700

    EndPoint

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Biodata</title>
+        <Style>
+
+        </Style>
+    </head>
+    <body>
+        <h5>NIS : 3103118103</h5>
+        <h5>Nama : Nur Endra Purwa Maulana Jati</h5>
+        <h5>Gender : Laki Laki</h5>
+        <h5>Phone : 081215730724</h5>
+        <h5>Kelas : XII RPL 3</h5>
+    </body>
+
+</html>
