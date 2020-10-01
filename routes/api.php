8edc6664c9dcc6ef43e4b055f628307165b81cb4
unknown <akiranation123@gmail.com>
2020-10-01 09:35:36 -0700

    autentikasi

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('book', 'BookController');
 Route::resource('author','AuthorController');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::get('user-profile', 'AuthController@userProfile');
+});
